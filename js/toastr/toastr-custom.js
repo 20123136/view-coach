72355acb329a4171a4d015565de0c91c3696e2ab
zhangna <zhangna@wilcom.com.cn>
2019-04-04 17:13:28 +0800

    注册OK

---
+++ js/toastr/toastr-custom.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zhangna on 2017/9/8.
+ *
+ * 该js是对自定义toastr组件的自定义js，用于所有页面的提示信息弹框。
+ */
+toastr.options = {
+    "closeButton": true,
+    "debug": true,
+    "progressBar": false,
+    "positionClass": "toast-right-center",
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut",
+    "preventDuplicates": true
+}
